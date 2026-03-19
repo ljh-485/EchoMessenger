686f8e8362820b9738b9ab8385ba33b190ceb9b7
정하 이 <ljhwsad200485@gmail.com>
2026-03-19 12:56:48 +0900

    과제 2 코딩과 테스트 완료 입력창 기존 메시지 지우기, 입력창에 입력 포커스 갖다 놓기, 엔터키로 전송하기, 입력방어

--- Form1.cs
+++ Form1.cs
@@ -16,9 +16,23 @@ namespace EchoMessenger
         private void bntSpend_Click(object sender, EventArgs e)
         {
             string typed_msg = txtInsert.Text;
-            lstTotalMessege.Items.Add(typed_msg);
-            txtInsert.Clear();
+           
+            
 
+            // 1. .Trim()을 사용하여 앞뒤 공백을 제거한 텍스트를 가져옵니다.
+            typed_msg = txtInsert.Text.Trim();
+
+            // 2. 내용이 비어있거나 공백만 있다면 전송하지 않고 종료(return)합니다.
+            if (string.IsNullOrWhiteSpace(typed_msg))
+            {
+                txtInsert.Clear(); // 스페이스바만 쳤을 경우를 대비해 칸을 비워줌
+                return;
+            }
+
+            // 3. 정상적인 내용이 있을 때만 리스트에 추가
+            lstTotalMessege.Items.Add(typed_msg);
+
+            txtInsert.Clear();
             //입력창으로 커서를 다시 보냅니다.
             txtInsert.Focus();
         }
@@ -37,7 +51,7 @@ namespace EchoMessenger
                 e.SuppressKeyPress = true;
 
                 // 이미 만들어둔 전송 버튼 클릭 이벤트를 실행
-                bntSpend_Click(this, new EventArgs());
+                bntSpend.PerformClick();
             }
         }
     }
