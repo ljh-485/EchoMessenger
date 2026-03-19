3a0183077494b09177cb0b8ded2c359d962e0372
정하 이 <ljhwsad200485@gmail.com>
2026-03-19 12:29:21 +0900

    과제1 코딩과 테스트 완료
    UI 구성, 전송 기능, 입력창 정리 완료

--- Form1.cs
+++ Form1.cs
@@ -9,14 +9,15 @@ namespace EchoMessenger
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            string typed_msg = txtInsert.Text;
-            lstTotalMessege.Items.Add(typed_msg);
-            txtInsert.Clear();
+            
+           
         }
 
         private void bntSpend_Click(object sender, EventArgs e)
         {
-
+            string typed_msg = txtInsert.Text;
+            lstTotalMessege.Items.Add(typed_msg);
+            txtInsert.Clear();
         }
 
         private void lstTotalMessege_SelectedIndexChanged(object sender, EventArgs e)
